8f84e6f49dfa907f64a8434bd474c2f93fe90e0f
BrunoPerez <brunoperez24793@gmail.com>
2024-04-29 16:03:34 -0300

    adding styles

--- src/components/PaginationTable/PaginationTable.jsx
+++ src/components/PaginationTable/PaginationTable.jsx
@@ -23,25 +23,25 @@ const PaginationTable = () => {
   const renderCharacterRows = () => {
     if (character.results) {
       return character.results.map((item) => (
-        <tr key={item.id} className='text-center h-[50px]'>
-          <td className=''>
+        <tr key={item.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <td className='w-1/3'>
             <img src={item.image} alt='foto' />
           </td>
           <td>{item.name}</td>
           <td>{item.species}</td>
           <td>{item.gender}</td>
         </tr>
-      )).slice((currentPage - 1) * 5, currentPage * 5); // Display only 5 rows per page
+      )).slice((currentPage - 1) * 5, currentPage * 5);
     }
     return null;
   }
 
   return (
 
-    <div className='flex flex-col justify-center items-center'>
+    <div className='relative overflow-x-auto p-5 bg-red-500 font-bold'>
 
-      <table className="w-1/2 text-sm text-left p-2 text-gray-500 dark:text-gray-400">
-        <thead className='text-xl bg-slate-300'>
+      <table className="w-full text-lg text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xl text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th className='p-2'>Image</th>
             <th className='p-2'>Name</th>
@@ -53,9 +53,10 @@ const PaginationTable = () => {
           {renderCharacterRows()}
         </tbody>
       </table>
-      <div className="my-5">
+      <div className="my-5 flex justify-center items-center">
+
         <ButtonComponent disabled={currentPage === 1} onClick={() => handlePageClick(currentPage - 1)} title='Previous' />
-        <span className='text-sky-950'>Page {currentPage}</span>
+        <span className='text-sky-950 font-bold'>Page {currentPage}</span>
         <ButtonComponent disabled={currentPage === (character.info ? character.info.pages : 1)}
           onClick={() => handlePageClick(currentPage + 1)} title='Next' />
 
